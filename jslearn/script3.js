1f06909b7e7916217a93b6b55413d9525875201c
Vaibhav Tiwari <vaibhavtiwari69691@gmail.com>
2026-03-03 15:34:41 +0000

    datatypes learning

---
+++ jslearn/script3.js
@@ -0,0 +1,12 @@
+// node jslearn/script3.js
+// arrays , objects
+const heros = ['thor', 'ironman'];//arrays
+let person = { //object
+    name :'vaibhav',
+    age : 19,
+    ishuman : true
+};
+const myfunction = function(){
+    console.log('hello world')
+}
+console.log(typeof heros);
