a4b79234f85d6ea36d820a09aa9891342b5bb568
Vaibhav Tiwari <vaibhavtiwari69691@gmail.com>
2026-03-03 07:36:22 +0000

    variables  learning

---
+++ jslearn/script.js
@@ -0,0 +1,11 @@
+const accountid = 32525;
+let accountemail = 'vaibhav@gmail.com';
+var accpass = '12345';
+usercity = 'navsari';
+console.table([accountemail,accpass,usercity]);
+/* we know that var ignores block scoped and 
+works fine while dealing with functonal scope 
+so we prefer not to use the var and we use let 
+and const
+node jslearn/script.js
+*/
