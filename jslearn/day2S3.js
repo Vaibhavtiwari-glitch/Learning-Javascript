d002101de8bc05435db27ae1c62df58888dc4cc9
Vaibhav Tiwari <vaibhavtiwari69691@gmail.com>
2026-03-04 15:26:46 +0000

    date time basics

---
+++ jslearn/day2S3.js
@@ -0,0 +1,13 @@
+// node jslearn/day2S3.js
+// let mydate = new Date()
+// //console.log(mydate.toString());
+// console.log(mydate.toTimeString());
+// let mycreatedate = new Date('2026-02-13');
+// console.log(mycreatedate.toString());
+// console.log(typeof mycreatedate);
+//let timer = Date.now();
+//console.log(timer)
+// let date = new Date('01-01-2026');
+// console.log(date.getTime());
+// console.log(date.getTime());
+console.log(Math.floor(Date.now()/1000));
