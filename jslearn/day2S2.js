98a64bd9f24e0d0e41146f413482790c9960065e
Vaibhav Tiwari <vaibhavtiwari69691@gmail.com>
2026-03-04 12:28:14 +0000

    maths and numbers

---
+++ jslearn/day2S2.js
@@ -0,0 +1,14 @@
+// node jslearn/day2S2.js
+// Maths and numbers in javascript
+const score = 5903
+const number = new Number(score);
+//console.log(number.toFixed(2));
+const pro = 210.832223;
+//console.log(pro.toPrecision(3));
+const soo = 1000000;
+//console.log(soo.toLocaleString('en-IN'));
+// console.log(number.toString());
+let ceil = console.log((Math.random()*10)+1);
+const min = 10 
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1)) + min )
