0ba0644ea455d08ef86ea9dfd2eab243ba770bbe
Vaibhav Tiwari <vaibhavtiwari69691@gmail.com>
2026-03-07 05:18:47 +0000

    scope basics

---
+++ jslearn/day5S1.js
@@ -0,0 +1,15 @@
+// node jslearn/day5S1.js
+let a = 300;
+if(true){
+let a = 10;
+const b = 20;
+console.log(a);
+}
+console.log(a);
+//console.log(b);
+/*
+var is accessable outside the block scope it means
+we can still print it outside the scope so thats the 
+problem we dont wanna face when we are coding so we
+simply use let and const 
+*/
